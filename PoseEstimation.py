97c85279607fec99b9a3c3d4369f1e66f2bbff78
Michele Cipriano <micco00x@gmail.com>
2018-07-25 14:57:32 +0200

    PoseEstimation.py not using metadata anymore

--- PoseEstimation.py
+++ PoseEstimation.py
@@ -46,14 +46,10 @@ def compute_pose_frame(input_image, sess):
     # Compute and draw the pose:
     vec = sess.run(net.get_output(name="concat_stage7"), feed_dict={"image:0": [image]})
     run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-    run_metadata = tf.RunMetadata()
     pafMat, heatMat = sess.run([net.get_output(name=last_layer.format(stage=stage_level, aux=1)),
                                 net.get_output(name=last_layer.format(stage=stage_level, aux=2))],
                                feed_dict={"image:0": [image]},
-                               options=run_options,
-                               run_metadata=run_metadata)
-    tl = timeline.Timeline(run_metadata.step_stats)
-    ctf = tl.generate_chrome_trace_format()
+                               options=run_options)
     heatMat, pafMat = heatMat[0], pafMat[0]
     humans = estimate_pose(heatMat, pafMat)
     pose_image = np.zeros(tuple(image.shape), dtype=np.uint8)
